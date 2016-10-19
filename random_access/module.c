9439daf0a4cedd99862ad1554705b47b23dad911
frank-desktop <592761667@qq.com>
2016-10-19 11:54:03 +0800

    Debug special case 'n=1' and 'ceil or floor'

--- random_access/module.c
+++ random_access/module.c
@@ -2,50 +2,57 @@
 #include <math.h>
 #include "myhead.h"
 
-extern int OCW; // initial window size
-extern int m; // # of contention window levels
-extern int N; // # of STAs need contend
-extern int n; // # of STAs sending BSR at current stage
-extern int M; // # of RUs for random access
+extern double OCW; // initial window size
+extern double m; // # of contention window levels
+extern double n; // # of STAs sending BSR at current stage
+extern double M; // # of RUs for random access
+extern double tau;
 
 /* for problem of r balls in n cells */
-double pmf_suc_contend(int k){
+double pmf_suc_contend(double k){ // k cells which only contain single ball
     /* compute the prob. */
     double p = 0.0; // just the prob.
-    int j = k;
-    int a1 = 0; // coefficient
+    double j = k;
+    double a1 = 0.0; // coefficient
     double a2 = 0.0;
     for (; j < M; j++){
-        a1 = combination_compute(j,k);
+        a1 = combination_compute((int) j,(int) k);
         a2 = S_j(j);
         p += pow(-1,k+j)*a1*a2; 
     }
-    printf("prob. of exact %d STAs contend success: %.3f\n", k, p);
+    // printf("prob. of exact %d STAs contend success: %.3f\n", (int)k, p);
     return p;
 }
 
-double S_j(int j){
+//* functions for pmf_suc_contend() *//
+double S_j(double j){
     double i = 0.0;
-    long int a1 = combination_compute(M,j);
+    long int a1 = combination_compute((int)M, (int)j);
     double s = a1;
     for (;i < j; i++){
-        s *= (n-i)/(M-i)*pow(1-1.0/(M-i), n-i-1);
+        s *= (ceil(n*tau)-i)/(M-i)*pow(1.0-1.0/(M-i), ceil(n*tau)-i-1); // n*tau is the actually contending STAs in a stage
     }
     return s;
 }
 
 /* for saturated analysis of 802.11ax random access */
 //* numerical method to find solution of equation groups *// // subsection-comment
-double solve_tau_p(int n, double Precise){
+double solve_tau_p(double Precise){
     double p = 0, tau = 0;
     double first = 0.0, last = 1.0, mid = 0.5; // dichotomy method  
     double f1 = 0.0, f2 = 0.0, f3 = 0.0; 
 
+    //* for special case n = 1 *//
+    if (n == 1){ // n=1, p = 0;
+        //printf("special case\n");
+        return 0;
+    }
+
     for (;;){
         mid = (first + last)/2.0;
-        f1 = equation (first, n);
-        f2 = equation (mid, n);
-        f3 = equation (last, n);
+        f1 = equation (first);
+        f2 = equation (mid);
+        f3 = equation (last);
         //
         if (fabs(f1) <= Precise){
             p = first;
@@ -73,8 +80,9 @@ double solve_tau_p(int n, double Precise){
     //printf("p:  %.6f; \ntau:    %.6f;\n", p, tau);
     return p;
 }
+
 //* group equations of tau and p *// // f = f1 - f2
-double equation(double p, int n){
+double equation(double p){
     double result = 0.0; 
     result = (1-pow(1.0-p,1.0/(n-1.0)))*M;
     double sum = 0.0;
@@ -85,4 +93,43 @@ double equation(double p, int n){
     result -= (OCW+1)/ (3*OCW -M +1 +(2+M)*sum);
 
     return result;
+}
+
+/* compute Ps & ns */
+/* calculate Ps & ns */ 
+/* Ps: prob. of success contend in a stage
+ * ns: # of STAs success contend in a stage
+ */
+//* compute Ps *//
+double compute_Ps(){
+    double prob_no_suc_stage = 0.0;
+    double prob_suc_stage = 0.0;
+
+    prob_no_suc_stage = pmf_suc_contend(0);
+    //printf("n= %.3f,  Ps(0): %.3f\n", n, prob_no_suc_stage);
+    prob_suc_stage = 1.0 - prob_no_suc_stage;
+    return prob_suc_stage;
+}
+
+//* compute ns *//
+double compute_ns(){
+    /* kmax is the up bound of k; */
+    double kmax = 0.0; // the most # of suc STAs in a stage
+    double num_suc_STA = 0.0;
+    if (M < n)
+        kmax = M;
+    else 
+        kmax = n;
+
+    int i = 1;
+    for (; i < kmax+1; i++){
+        num_suc_STA += i*pmf_suc_contend(i);
+    }
+    //printf("expected # of success STAs in a stage = %.3f\n", num_suc_STA);
+    return num_suc_STA;
+}
+
+//* compute # of stages until suc stage *//
+double compute_stages_suc(double prob_suc_stage){ 
+    return 1.0/prob_suc_stage;
 }
--- random_access/myhead.h
+++ random_access/myhead.h
@@ -1,9 +1,17 @@
 #include "my_math.h"
 /* problem of r balls in n cells */
-double S_j(int j); // prob. of at least j STAs who success contend
-double pmf_suc_contend(int k); // prob. of exact k STAs who success contend
+double S_j(double j); // prob. of at least j STAs who success contend
+double pmf_suc_contend(double k); // prob. of exact k STAs who success contend
+
 /* for saturated analysis of random access */
-double solve_tau_p(int, double);
-double equation(double p, int num_sta);
-double solve_throughput(double tau, int num_sta);
-double solve_energy_efficiency(int num_sta);
+double solve_tau_p(double Precise);
+double equation(double p);
+
+/* calculate Ps & ns & level */ 
+/* Ps: prob. of success contend in a stage
+ * ns: # of STAs success contend in a stage
+ * stages_suc: # stages until a suc stage 
+ */
+double compute_Ps();
+double compute_ns();
+double compute_stages_suc(double prob_suc_stage); 
